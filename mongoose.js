601ca6c4193ee0f917d44f5adfcc45884f1b63e3
andrumozo97 <andrumozo97@gmail.com>
2020-06-03 21:52:32 -0300

    User funcionando, Payments escrito(no probado)

--- mongoose.js
+++ mongoose.js
@@ -11,6 +11,7 @@ const url = 'mongodb://cliente:cliente@localhost:27017/' + dbName; //aca va el u
 const client = new MongoClient(url, {useUnifiedTopology : true} );  // el segundo parametro me lo pide la consola porque hay algo deprecado
 console.log("cliente creado,  conectando...");
 
+//Declaramos los esquemas
 var userSchema = new Schema({ // define a schema
     nombre: {type : String },
     apellido: {type : String },
@@ -21,23 +22,53 @@ var userSchema = new Schema({ // define a schema
     pagos: {type : [] }
     });
 
-/*
-    userSchema.statics.findPorDNI = function (dni , callback){
-        return this.find( {dni: new RegExp(dni , 'i') }, callback)
-    }
-*/
+var paymentSchema = new Schema({
+    id_pago: {type : Number },
+    dni: {type : Number },
+    fecha: {type : Date },
+    clase :{type : String },
+    monto: {type : Number },
+})
+
+//Declaramos las queris y las funciones para los usuarios
 
 userSchema.query.porNombre = function(name) {
     return this.where({ nombre: new RegExp(name, 'i') });
 };
 
-userSchema.statics.findPorNombre = function (nombre , callback){
-    console.log("Entro al metodoo")
-    return this.find( {nombre: "Roger"/*new RegExp(nombre, 'i')*/ }, callback) //VER BIEN Q ES ESTO DE RegExp
+userSchema.statics.findPorNombre = function (name , callback){
+    return this.find( {nombre: new RegExp(name, 'i') }, callback) //VER BIEN Q ES ESTO DE RegExp
 }
 
-var User = mongoose.model('User', userSchema);
+userSchema.statics.deletePorDni = function (_dni , callback){
+    console.log(_dni)
+    return this.deleteMany({ dni:_dni }, callback)
+}
 
+userSchema.statics.modifyPorDni = function (_dni , new_atts, callback){
+    return this.findOneAndUpdate({dni: _dni } ,{$set: new_atts}, callback) //busca por dni y actualiza el att por new_value 
+}
+
+
+//Declaramos las funciones para el esquema payment
+paymentSchema.statics.findPago = function (_id_pago , callback){
+    return this.find( {id : _id_pago }, callback)
+}
+
+paymentSchema.statics.ModifyPago = function (_id_pago ,new_atts, callback){
+    return this.findOneAndUpdate({id: _id_pago } ,{$set: new_atts}, callback)
+}
+
+paymentSchema.statics.deletePago = function (_id_pago , callback){
+    return this.deleteMany( {id_pago : _id_pago }, callback)
+}
+
+
+//Modelamos los esquemas
+var User = mongoose.model('User', userSchema);
+var Payment = mongoose.model('Payment' , paymentSchema)
+
+//Usuarios
 var new_user1 = new User({
     nombre: 'Lionel',
     apellido: 'Messi',
@@ -68,6 +99,19 @@ var new_user3 = new User({
     pagos: [80,8080,80000]
 });
 
+//Pagos
+var pay_new = new Payment({
+    id_pago: 443,
+    dni: "246",
+    fecha: new Date(),
+    clase :"Pilates",
+    monto: 500
+})
+
+
+
+const nuevosValores = {nombre: 'Roman'};
+
 
 mongoose.connect(url, function (err) {
     if (err) throw err;
@@ -80,186 +124,29 @@ mongoose.connect(url, function (err) {
                 if (err) {
                     console.log(err);
                 } else {
-                    console.log("Agrego al usuario correctamente 2");
                     User.find().porNombre("Lionel").exec(function(err, usuarios) {
-                        console.log("Entro")
                         console.log(usuarios);
+                        User.findPorNombre('Tomas', function (err, usuarios) {
+                            console.log(usuarios);
+                            User.modifyPorDni(10101010,nuevosValores,function(err,user){
+                                console.log(user);
+                                User.findPorNombre('Roman', function (err, usuarios){
+                                    console.log("Entro a ver si lo modifico por roman");
+                                    console.log(usuarios);
+                                    User.deletePorDni(10101010,function(err,result){
+                                        console.log("Entro a borrar")
+                                        console.log(result)
+                                        mongoose.connection.close();
+
+                                    })
+                                });                               
+                        
+                            });
+                        });
+
                     });
-                      mongoose.connection.close();
-                    }});
-                    
                 }
             });
         }
-    );
-
-
-
-
-/*
-//Create Read Update Delete (en user)
-//create
-const createUser = function(db,newUser,callback){
-    const users = db.collection('users');
-    users.insertOne(newUser, function(err, result){
-        assert.equal(err, null);
-        assert.equal(result.insertedCount, 1);
-        console.log("User added succesfully");
-        callback(result);
-    });  
-}
-//read
-const findUsers = function(db, callback){
-    const collection_usuarios = db.collection('users');
-    //aca en el find podriamos poner el criterio de busqueda, que se pase por parametros
-    collection_usuarios.find({}).toArray(function(err, elementos){ 
-        assert.equal(err, null);
-        console.log(elementos);
-        callback(elementos);
     })
-}
-
-const findUser = function(db,criterioBusqueda, callback){
-    const collection_usuarios = db.collection('users');
-    collection_usuarios.find(criterioBusqueda).toArray(function(err, elementos){ 
-        assert.equal(err, null);
-        console.log(elementos);
-        callback(elementos);
-    })
-}
-
-const findGeneric = function(db,nombreColeccion,criterioBusqueda, callback){
-    const collection = db.collection(nombreColeccion);
-    collection.find(criterioBusqueda).toArray(function(err, elementos){ 
-        assert.equal(err, null);
-        console.log(elementos);
-        callback(elementos);
-    })
-}
-
-//update
-//newAtts serian los atributos nuevos, y updateAtts aquellos por los que se encuentra la(s) tupla(s) que se actualizaran.
-const updateUser = function(db, newAtts, updateCriteria, callback) {
-    const users = db.collection('users');
-    users.updateMany(updateCriteria, {$set: newAtts}, function(err, result){
-        assert.equal(err, null);
-        console.log(result.modifiedCount + " document(s) updated");
-        callback(result);
-    })
-}
-
-//delete
-
-const deleteUser = function(db, deleteCriteria, callback){
-    const users = db.collection('users');
-    users.deleteMany(deleteCriteria, function(err, result){
-        assert.equal(err, null);
-        console.log(result.deletedCount + "documents deleted");
-        callback(result);
-    })
-}
-
-
-const user_new = {
-    nombre: 'Andrelo',
-    apellido: 'Mozo',
-    peso: '10',
-    dni: '12323321',
-    fechaInicio: new Date(),
-    clases: ['Musculacion'],
-    pagos: [7,9,12]
-}
-
-let user_new_2 = {
-    nombre: 'Tomas',
-    apellido: 'Castilla',
-    peso: '10',
-    dni: '666999',
-    fechaInicio: new Date(),
-    clases: ['Musculacion', 'Crossfit'],
-    pagos: [4,8,29]
-}
-
-let criterioTomas ={nombre: "Tomas"} 
-
-let criterioAndrelo = { nombre: "Andrelo"
-}
-let criterioAndres = { nombre: "Andres" }
-
-const nuevosValores = {nombre: 'Andres'};
-const criterioUpdate = {nombre: 'Andrelo'};
-
-//Ahora operamos sobre la coleccion de pagos
-
-let user_new_3 = {
-    nombre: 'Juan',
-    apellido: 'Gonzalez',
-    peso: '10',
-    dni: '246',
-    fechaInicio: new Date(),
-    clases: ['Pilates'],
-    pagos: [56]
-}
-
-let pay_new = {
-    id_pago: 443,
-    dni: "246",
-    fecha: new Date(),
-    clase :"Pilates",
-    monto: 500
-}
-
-
-const insertPayment = function(db,newPayment,callback){
-    const payments = db.collection('payments');
-    payments.insertOne(newPayment, function(err, result){
-        assert.equal(err, null);
-        assert.equal(result.insertedCount, 1);
-        console.log("Payments added succesfully");
-        
-        //Ahora agregamos el pago al usuario
-        const users = db.collection('users');
-        users.updateOne({ dni : newPayment.dni} , {$push: {pagos: newPayment.id_pago}},function(err, result){
-            assert.equal(err, null);
-            console.log(result.modifiedCount + " document(s) updated");
-            callback(result);
-        })
-      //  callback(result); //CREO Q ESTA NO VA
-    });  
-}
-
-
-// Use connect method to connect to the Server
-client.connect(function (err) {
-    console.log("antes del assertion");
-    assert.equal(null, err);
-    console.log("Connected correctly to server");
-    const db = client.db(dbName);
-    createUser(db, user_new, function(){
-        createUser(db,user_new_2,function(){
-            findUsers(db, function(){
-                console.log("Ahora utilizamos el find con un criterio por parametro")
-                findUser(db,criterioTomas,function(){
-                    console.log("Ahora buscamos a tomas con el find generico")
-                    findGeneric(db,"users",criterioTomas,function(){
-                        console.log("Ahora probamos el update sobre andrelo para cambiarlo a andres")
-                        updateUser(db,nuevosValores,criterioUpdate,function(){
-                            console.log("Ahora eliminamos a Andres")
-                            deleteUser(db,criterioAndres,function(){
-                                console.log("Probamos la adicion de pagos")
-                                createUser(db,user_new_3, function(){
-                                    insertPayment(db,pay_new,function(){
-                                        client.close();
-                                    })
-                                }) 
-                            })
-                        })
-                        
-                    })
-                })
-            })
-        })
-    })
-    
-});
-*/
+})
